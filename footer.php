e3d1e3910d0c2fd53a7b702864d349d46a2b6688
AleksK13 <aleksandr.kulsa@gmail.com>
2020-03-05 21:01:54 +0200

    Footer

--- footer.php
+++ footer.php
@@ -1,6 +1,34 @@
     
     <!-- Čia pasikartojanti visuose puslapiuose apatinė dalis -->
 
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Musu paskaitos</h5>
+                    <p class="grey-text text-lighten-4">Cia galima paziuret ka darem per paskaitas.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3"
+                                href="https://www.delfi.lt/">Delfi</a></li>
+                        <li><a class="grey-text text-lighten-3"
+                                href="D:\Programavimas\Web 02-25\2.CSS\index.html">CSS</a></li>
+                        <li><a class="grey-text text-lighten-3"
+                                href="D:\Programavimas\Web 02-25\1.HTML\index.html">HTML</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © Future Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="scripts/script.js"></script>
 
