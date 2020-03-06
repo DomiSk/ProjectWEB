b7cb6a2d2c78c350bf86dccd9b2bc16fe1866ddd
audrius707 <audrius.bingelis@gmail.com>
2020-03-06 20:29:19 +0200

    audrius

--- audrius.php
+++ audrius.php
@@ -1,35 +1,4 @@
-<!DOCTYPE html>
-<html lang="lt">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Kelionės Lietuvoje</title>
-  
-  <link rel="stylesheet" href="css/css.css">
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-
-<body>
-  
-
-  
-  <nav>
-    
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo">Keliauk ir pažink Lietuvą.</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a
-            href ="https://www.google.com/maps/d/viewer?fbclid=IwAR0F83ycqnO_B1gmV0MTZ1pCQxu09mShIaiPNJnnWcEOL6i1c9HfjVtja9Y&mid=1R_HtxcQ7eSnQy0vbLWNn1WA1RPKtf0oc&ll=55.55081308862803%2C22.953983739062437&z=7">Žemėlapis</a>
-        </li>
-        <li><a href="badges.html">Kontaktai</a></li>
-        
-        
-      </ul>
-    </div>
-  </nav>
+<?php include "header.php" ?>
 
 
   <div>
@@ -104,35 +73,4 @@
     </div>
   </div>
 
-
-  <footer class="page-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text">Kelionės Lietuvoje</h5>
-          <p class="grey-text text-lighten-4">Kelionės po Lietuvą, tai puikus pasirinkimas norint sužinoto daugiau apie savo šalį arba supažindinti svečius. Planuokite keliones Lietuvoje jau dabar.</p>
-        </div>
-        <div class="col l4 offset-l2 s12">
-          <h5 class="white-text">Naudinga informacija keliaujantiems :</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="https://www.citybee.lt/lt">Automobilių nuoma</a></li>
-            <li><a class="grey-text text-lighten-3" href="https://traukiniobilietas.lt/portal/">Traukinių bilietai</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-        © 2020 Copyright Text
-        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-      </div>
-    </div>
-  </footer>
-
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script src="scripts/script.js"></script>
-
-</body>
-
-</html>
+  <?php include "footer.php" ?>
