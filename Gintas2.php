e3c51e2bc890df15a5e1eb2cb835fa24c4ff6c41
G1ntautas <info@gloma.lt>
2020-03-06 21:52:01 +0200

    GINTAUTAS puslapio darbai

---
+++ Gintas2.php
@@ -0,0 +1,78 @@
+<?php include "header.php" ?>
+
+<div class="nav-wrapper container">
+    
+
+    <h1></h1>
+
+    <h1 class=" text-lighten-2 center-align">Gintauto pomėgiai</h1>
+
+
+
+
+
+    <div class="grid-container">
+        <div class="a1"><i class="large material-icons red-text">directions_bike</i></div>
+        <div class="a2">
+        <i class="large material-icons red-text">directions_bike</i></h4>
+        </div>
+        <div class="a3">
+        <i class="large material-icons red-text">directions_bike</i></p>
+        </div>
+        <div class="a4">
+            <h4>Keliavimas dviračiu</h4>
+    </div>
+        <div class="a5">
+            <h4>Keliavimas dviračiu sdfsdfsdf sdfsdfsdfsdf sdfsfsdfsdf </h4>
+        </div>
+        <div class="a6">
+        <h4>Keliavimas dviračiu</h4>
+        </div>
+        <div class="a7">
+        <p>Dviratis - transporto priemonė, kurią sudaro du ratai ir ji juda nuo žmogaus jėgos, perduodamos minant pedalus.</p>
+        </div>
+        <div class="a8">
+        <p>Dviratis - transporto priemonė, kurią sudaro du ratai ir ji juda nuo žmogaus jėgos, perduodamos minant pedalus.</p>
+        </div>
+        <div class="a9">
+            <p>Dviratis - transporto priemonė, kurią sudaro du ratai ir ji juda nuo žmogaus jėgos, perduodamos minant pedalus.</p>
+        </div>
+    </div>
+
+
+    <div class="row">
+
+        <div class="col s12 m4 center-align">
+
+            <i class="large material-icons red-text">directions_bike</i>
+            <h4>Keliavimas dviračiu</h4>
+            <p>Dviratis - transporto priemonė, kurią sudaro du ratai ir ji juda nuo žmogaus jėgos, perduodamos minant pedalus.</p>
+
+
+
+
+        </div>
+        <div class="col s12 m4 center-align">
+
+            <i class="large material-icons green-text">directions_walk</i>
+            <h4>Pėsčiųjų žygiai</h4>
+            <p>Ėjimas - žmogaus judėjimas, naudojant kojas, bet kartais į pagalbą būna pasitelkiamos ir rankos. </p>
+
+        </div>
+        <div class="col s12 m4 center-align">
+
+            <i class="large material-icons blue-text">rowing</i>
+            <h4>Plaukimas baidarėmis</h4>
+            <p>Baidarė - nedidelis laivelis, varomas irklais, dažniausiai vandens paviršiumi, kuriems jėga perduodama žmogaus rankomis.</p>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+
+
+<?php include "footer.php" ?>
