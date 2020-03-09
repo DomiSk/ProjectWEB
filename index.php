d3381bd63e422d5c6d91b4bd7f3d71327cad0794
AleksK13 <aleksandr.kulsa@gmail.com>
2020-03-09 13:55:22 +0200

    paspaudziant an nuotrauku atidaro jusu sukurta puslapi

--- index.php
+++ index.php
@@ -30,63 +30,71 @@
 
 </div>
 <div class="container">
-<section class="firsp1">
+  <section class="firsp1">
 
-  <h2 class="center-align md">Musu darbai</h2>
+    <h2 class="center-align md">Musu darbai</h2>
 
-  <div class="flexbo ">
-    <div>
-      <img src="https://picsum.photos/200" alt="">
-      <h3>Aleks</h3>
-      <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
-        officiis
-        nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
-        repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Excepturi
-        vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
-        maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
-    </div>
-    <div>
-      <img src="https://picsum.photos/201" alt="">
-      <h3>Denis</h3>
-      <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
-        officiis
-        nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
-        repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Excepturi
-        vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
-        maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
-    </div>
-</section>
+    <div class="flexbo ">
+      <div>
+        <a href="audrius.php">
+          <img src="https://picsum.photos/200" alt="" style="">
+        </a>
+        <h3>Audrius</h3>
+        <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
+          officiis
+          nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
+          repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Excepturi
+          vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
+          maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
+      </div>
+      <div>
+        <a href="dominykas.php">
+          <img src="https://picsum.photos/199" alt="" style="">
+        </a>
+        <h3>Dominykas</h3>
+        <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
+          officiis
+          nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
+          repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Excepturi
+          vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
+          maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
+      </div>
+  </section>
 
-<section class="firsp1">
-  
-  <div class="flexbo">
-    <div>
-      <img src="https://picsum.photos/200" alt="">
-      <h3>Aleks</h3>
-      <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
-        officiis
-        nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
-        repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Excepturi
-        vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
-        maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
-    </div>
-    <div>
-      <img src="https://picsum.photos/201" alt="">
-      <h3>Denis</h3>
-      <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
-        officiis
-        nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
-        repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Excepturi
-        vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
-        maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
-    </div>
+  <section class="firsp1">
 
-  </div>
-</section>
+    <div class="flexbo">
+      <div>
+        <a href="Gintas.php">
+          <img src="https://picsum.photos/201" alt="" style="">
+        </a>
+        <h3>Gintautas</h3>
+        <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
+          officiis
+          nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
+          repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Excepturi
+          vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
+          maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
+      </div>
+      <div>
+        <a href="aleksandr.php">
+          <img src="https://picsum.photos/202" alt="" style="">
+        </a>
+        <h3>Aleksandr</h3>
+        <p:>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi vitae amet quia dicta
+          officiis
+          nemo consectetur aperiam, veniam eum sit, corporis exercitationem maiores totam repudiandae quas
+          repellat. Velit, est suscipit?Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Excepturi
+          vitae amet quia dicta officiis nemo consectetur aperiam, veniam eum sit, corporis exercitationem
+          maiores totam repudiandae quas repellat. Velit, est suscipit?</p:>
+      </div>
+
+    </div>
+  </section>
 </div>
 
 
