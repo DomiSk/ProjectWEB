22e1b976cdaf4a38d0f8a6d4a5734237fa85eaee
audrius707 <audrius.bingelis@gmail.com>
2020-03-09 20:04:44 +0200

    aa

--- audrius.php
+++ audrius.php
@@ -14,7 +14,7 @@
 
 
 
-        <div class="row">
+        <div class="row myflex">
             <div class="center-align col s12 m4 ">
 
                 <a href="https://www.citybee.lt/lt/transportas"><i class="spalva large material-icons text">directions_car</i></a>
@@ -24,9 +24,24 @@
                 </h4>
 
                 <p>
-                    <form target="_blank" action="https://www.google.com/maps/dir///@54.6818571,25.2358156,12z/data=!4m2!4m1!3e0">
-                        <input type class="text-colour spalva" placeholder="Kur keliausite....." name="search">
-                        <button>Ieškoti</button>
+                    <!-- <form target="_blank" action="https://www.google.com/maps/dir///@54.6818571,25.2358156,12z/data=!4m2!4m1!3e0"> -->
+                        <!-- <input type class="text-colour spalva"  name="search"> -->
+                    <button onclick="myFunction()">Ieskoti</button>
+                    <input  id="googlemap"  type="text" class="text-colour spalva" >
+
+
+                    <script>
+                        function myFunction() {
+
+                            var inputValue = document.querySelector("#googlemap").value;
+
+
+                            var pavadinimas = inputValue;
+                            window.location.href = "https://www.google.com/maps/place/" + pavadinimas;
+                        }
+                    </script>
+
+
                     </form>
 
                 </p>
@@ -112,51 +127,51 @@
         <div class="container  ">
             <div class="row">
                 <div class="center-align col s2">
-                  <a href="https://www.piliakalniai.lt/">
-                  <h6>  Piliakalniai
-                    <img src="https://picsum.photos/150/100" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.piliakalniai.lt/">
+                        <h6> Piliakalniai
+                            <img src="https://picsum.photos/150/100" alt="">
+                        </h6>
+                    </a>
                 </div>
                 <div class="center-align col s2 ">
-                  <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                  <h6>  Pilys ir dvarai.
-                    <img src="https://picsum.photos/150/102" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
+                        <h6> Pilys ir dvarai.
+                            <img src="https://picsum.photos/150/102" alt="">
+                        </h6>
+                    </a>
                 </div>
                 <div class="center-align col s2 ">
-                  <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                  <h6>  Apžvalgos bokštai.
-                    <img src="https://picsum.photos/150/101" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
+                        <h6> Apžvalgos bokštai.
+                            <img src="https://picsum.photos/150/101" alt="">
+                        </h6>
+                    </a>
                 </div>
                 <div class="center-align col s2 ">
-                  <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                  <h6>  Muziejai.
-                    <img src="https://picsum.photos/150/103" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
+                        <h6> Muziejai.
+                            <img src="https://picsum.photos/150/103" alt="">
+                        </h6>
+                    </a>
                 </div>
                 <div class="center-align col s2 ">
-                  <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                  <h6>  Pažintiniai takai.
-                    <img src="https://picsum.photos/150/99" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
+                        <h6> Pažintiniai takai.
+                            <img src="https://picsum.photos/150/99" alt="">
+                        </h6>
+                    </a>
                 </div>
                 <div class="center-align col s2 ">
-                  <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                  <h6>  Kitos vietos.
-                    <img src="https://picsum.photos/149/100" alt="">
-                    </h6>
-                  </a>  
+                    <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
+                        <h6> Kitos vietos.
+                            <img src="https://picsum.photos/149/100" alt="">
+                        </h6>
+                    </a>
                 </div>
-                
-                
 
-               
+
+
+
             </div>
         </div>
     </div>
--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
   });
+
+
+ 
+
+        function myFunction() {
+
+            var inputValue = document.querySelector("input").value;
+
+
+            var pavadinimas = inputValue;
+            window.location.href = "https://www.google.com/maps/place/" + pavadinimas;
+        }
+
+   
