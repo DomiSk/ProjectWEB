106bb4caf9d79c5075486a7de4d2b784e186db2f
audrius707 <audrius.bingelis@gmail.com>
2020-03-10 21:49:47 +0200

    naujas

--- audrius1.php
+++ audrius1.php
@@ -121,46 +121,46 @@
 
             <div class="row myflex">
 
-                <div class="center-align col s2">
+                <div class="center-align col s6 m4 l2">
                     <a href="https://www.piliakalniai.lt/">
-                        <h6> Piliakalniai
+                        <h6> Piliakalniai</h6>
                             <img src="https://picsum.photos/150/100" alt="">
-                        </h6>
+                        
                     </a>
                 </div>
-                <div class="center-align col s2 ">
+                <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Pilys ir dvarai.
+                        <h6> Pilys ir dvarai</h6>
                             <img src="https://picsum.photos/150/102" alt="">
-                        </h6>
+                        
                     </a>
                 </div>
-                <div class="center-align col s2 ">
+                <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Apžvalgos bokštai.
+                        <h6>Apžvalgos bokštai</h6>
                             <img src="https://picsum.photos/150/101" alt="">
-                        </h6>
+                        
                     </a>
                 </div>
-                <div class="center-align col s2 ">
+                <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Muziejai.
+                        <h6> Muziejai</h6> 
                             <img src="https://picsum.photos/150/103" alt="">
-                        </h6>
+                       
                     </a>
                 </div>
-                <div class="center-align col s2 ">
+                <div class="center-align col s6 m4 l2 ">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Pažintiniai takai.
+                        <h6> Pažintiniai takai</h6>
                             <img src="https://picsum.photos/150/99" alt="">
-                        </h6>
+                        
                     </a>
                 </div>
-                <div class="center-align col s2 ">
+                <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Kitos vietos.
+                        <h6> Kitos vietos </h6>
                             <img src="https://picsum.photos/149/100" alt="">
-                        </h6>
+                       
                     </a>
                 </div>
 
