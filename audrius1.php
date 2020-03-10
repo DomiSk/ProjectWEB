34fc9499bac43bce23cded753033ce2bed071723
audrius707 <audrius.bingelis@gmail.com>
2020-03-10 22:09:51 +0200

    nn

--- audrius1.php
+++ audrius1.php
@@ -69,7 +69,7 @@
 
                         Keliauk laivu.
 
-                        </h4>
+                    </h4>
 
                 </a>
 
@@ -96,15 +96,15 @@
 
 
             <script>
-            //     function myFunction() {
+                //     function myFunction() {
 
-            //         var inputValue = document.querySelector("#googlemap").value;
+                //         var inputValue = document.querySelector("#googlemap").value;
 
 
-            //         var pavadinimas = inputValue;
-            //         window.location.href = "https://www.google.com/maps/place/" + pavadinimas;
-            //     }
-            // 
+                //         var pavadinimas = inputValue;
+                //         window.location.href = "https://www.google.com/maps/place/" + pavadinimas;
+                //     }
+                // 
             </script>
 
 
@@ -124,43 +124,43 @@
                 <div class="center-align col s6 m4 l2">
                     <a href="https://www.piliakalniai.lt/">
                         <h6> Piliakalniai</h6>
-                            <img src="https://picsum.photos/150/100" alt="">
-                        
+                        <img class="responsive-img" src="https://picsum.photos/150/100" alt="">
+
                     </a>
                 </div>
                 <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
                         <h6> Pilys ir dvarai</h6>
-                            <img src="https://picsum.photos/150/102" alt="">
-                        
+                        <img class="responsive-img" src="https://picsum.photos/150/102" alt="">
+
                     </a>
                 </div>
                 <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
                         <h6>Apžvalgos bokštai</h6>
-                            <img src="https://picsum.photos/150/101" alt="">
-                        
+                        <img class="responsive-img" src="https://picsum.photos/150/101" alt="">
+
                     </a>
                 </div>
                 <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
-                        <h6> Muziejai</h6> 
-                            <img src="https://picsum.photos/150/103" alt="">
-                       
+                        <h6> Muziejai</h6>
+                        <img class="responsive-img" src="https://picsum.photos/150/103" alt="">
+
                     </a>
                 </div>
                 <div class="center-align col s6 m4 l2 ">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
                         <h6> Pažintiniai takai</h6>
-                            <img src="https://picsum.photos/150/99" alt="">
-                        
+                        <img class="responsive-img" src="https://picsum.photos/150/99" alt="">
+
                     </a>
                 </div>
                 <div class="center-align col s6 m4 l2">
                     <a href="https://www.15min.lt/pasaulis-kiseneje/naujiena/per-lietuva/10-dvaru-lietuvoje-kurie-praejusiais-metais-keliautojus-domino-labiausiai-642-1090768">
                         <h6> Kitos vietos </h6>
-                            <img src="https://picsum.photos/149/100" alt="">
-                       
+                        <img class="responsive-img" src="https://picsum.photos/149/100" alt="">
+
                     </a>
                 </div>
 
