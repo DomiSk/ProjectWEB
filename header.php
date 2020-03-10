2bfecd1d41545011337273185d1e887ce1ac14ba
AleksK13 <aleksandr.kulsa@gmail.com>
2020-03-10 19:50:01 +0200

    search button in header

--- header.php
+++ header.php
@@ -1,41 +1,46 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" href="styles/style.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="styles/style.css">
 
 
-    <title>Projekto pavadinimas</title>
+  <title>Projekto pavadinimas</title>
 </head>
 
 <body>
 
-<nav>
+  <nav>
     <div class="nav-wrapper container">
       <a href="#!" class="brand-logo">Logo</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
       <ul class="right hide-on-med-and-down">
+        <li>
+          <form action="/action_page.php">
+            <input id="search-input" type="text" placeholder="Search.." name="search">
+            <button class="butt" type="submit"><i class="material-icons">search</i></button>
+          </form>
+        </li>
         <li><a href="index.php">Home</a></li>
         <li><a href="https://www.vilniuscoding.lt/">VCS</a></li>
         <li><a href="mobile.html">Contacts</a></li>
       </ul>
+
     </div>
   </nav>
 
   <ul class="sidenav" id="mobile-demo">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">Javascript</a></li>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="https://www.vilniuscoding.lt/">VCS</a></li>
     <li><a href="mobile.html">Mobile</a></li>
   </ul>
- 
-    <!-- Čia pasikartojanti visuose puslapiuose viršutinė dalis -->
+
+  <!-- Čia pasikartojanti visuose puslapiuose viršutinė dalis -->
